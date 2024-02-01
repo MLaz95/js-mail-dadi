34970e187a2acdfa83b2dd1e1ba16706d19fcfa0
marco <lazzari.marco.ml@gmail.com>
2024-02-01 16:57:20 +0100

    script base funziona

--- mail/script.js
+++ mail/script.js
@@ -2,6 +2,7 @@
 const authorizedList = ['pinco.pallo@gmail.com', 'jane.doe@gmail.com', 'erika@proton.me'];
 console.log(authorizedList);
 
+// records all needed html elements
 const emailElement = document.querySelector("#email");
 const buttonElement = document.querySelector("#button");
 const resultElement = document.querySelector("#result")
@@ -10,10 +11,21 @@ const resultElement = document.querySelector("#result")
 buttonElement.addEventListener('click',
     function(){
         const userInput = emailElement.value;
-        console.log(userInput)
+        // sets up a variable to determine if the user email matches
+        let accessGranted = false;
+        
+        // runs through every email in the array and compares with the user input
+        for(i = 0; i < authorizedList.length; i++){
+            if(userInput == authorizedList[i]){
+                console.log(`${userInput} valid`)
+                // if there is a match, the access variable is set to true
+                accessGranted = true;
+            }
+        }
+        // after comparing the user info with all the emails on the list
+        // if the access variable is still false then access is denied
+        if(accessGranted == false){
+            console.log(`${userInput} denied`);
+        }
     }
-    )
-
-// see if it matches any emails in the array
-// communicate the result
-
+)
