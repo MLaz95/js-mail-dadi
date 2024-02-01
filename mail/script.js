79ed4a81926366d3eb1ae378f44c06c78e8f3c4a
marco <lazzari.marco.ml@gmail.com>
2024-02-01 16:32:06 +0100

    iniziato esercizio mail

---
+++ mail/script.js
@@ -0,0 +1,7 @@
+// create an array of known emails
+const authorizedList = ['pinco.pallo@gmail.com', 'jane.doe@gmail.com', 'erika@proton.me']
+console.log(authorizedList)
+// ask the user for their email
+// see if it matches any emails in the array
+// communicate the result
+
