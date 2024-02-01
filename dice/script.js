80e1ec9f0a8b328c032c41a529e41a5811bb85dd
marco <lazzari.marco.ml@gmail.com>
2024-02-01 16:19:44 +0100

    aggiunto stile; dadi completati

--- dice/script.js
+++ dice/script.js
@@ -1,4 +1,7 @@
 const buttonElement = document.querySelector("#button")
+const playerElement = document.querySelector("#player")
+const computerElement = document.querySelector("#computer")
+const resultElement = document.querySelector("#result")
 
 buttonElement.addEventListener('click',
     function() {
@@ -6,12 +9,15 @@ buttonElement.addEventListener('click',
         const computerRoll = (Math.floor(Math.random() * 6) + 1);
 
         console.log(`You rolled a ${playerRoll}, the computer rolled a ${computerRoll}.`)
+        playerElement.innerHTML = playerRoll
+        computerElement.innerHTML = computerRoll
+
         if(playerRoll == computerRoll){
-            console.log("That's a tie!")
+            resultElement.innerHTML = "It's a tie!"
         } else if(playerRoll > computerRoll){
-            console.log("you won!")
+            resultElement.innerHTML = "You won!"
         } else{
-            console.log("you lost")
+            resultElement.innerHTML = "You lost!"
         }
 
 
