3cb77e84a0d05f8b593a2437cbe29eea41b3f784
marco <lazzari.marco.ml@gmail.com>
2024-02-01 16:37:24 +0100

    script legge user input correttamente

--- mail/script.js
+++ mail/script.js
@@ -1,7 +1,19 @@
 // create an array of known emails
-const authorizedList = ['pinco.pallo@gmail.com', 'jane.doe@gmail.com', 'erika@proton.me']
-console.log(authorizedList)
+const authorizedList = ['pinco.pallo@gmail.com', 'jane.doe@gmail.com', 'erika@proton.me'];
+console.log(authorizedList);
+
+const emailElement = document.querySelector("#email");
+const buttonElement = document.querySelector("#button");
+const resultElement = document.querySelector("#result")
+
 // ask the user for their email
+buttonElement.addEventListener('click',
+    function(){
+        const userInput = emailElement.value;
+        console.log(userInput)
+    }
+    )
+
 // see if it matches any emails in the array
 // communicate the result
 
